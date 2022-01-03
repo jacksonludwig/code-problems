dd4b4fa15bea1a5ed8ac83999247618486b7f017
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-01-03 12:46:06 -0500

    rename funcs

--- townjudge/lib/townjudge.ex
+++ townjudge/lib/townjudge.ex
@@ -1,5 +1,5 @@
 defmodule Townjudge do
-  defp check_if_trusting_person(i, trust_pair) do
+  defp is_trusting_person(i, trust_pair) do
     [_truster, trustee] = trust_pair
     i == trustee
   end
@@ -9,15 +9,15 @@ defmodule Townjudge do
     truster
   end
 
-  defp check_if_trusted_by_all(i, trust_pairs, n) when i > 0 do
+  defp is_trusted_by_all(i, trust_pairs, n) when i > 0 do
     Enum.to_list(1..n) -- [i] ==
       trust_pairs
-      |> Enum.filter(&check_if_trusting_person(i, &1))
+      |> Enum.filter(&is_trusting_person(i, &1))
       |> Enum.map(&pair_to_truster(&1))
       |> Enum.sort()
   end
 
-  defp check_if_not_trusting_others(i, trust_pairs) do
+  defp is_not_trusting_others(i, trust_pairs) do
     result =
       trust_pairs
       |> Enum.map(&pair_to_truster(&1))
@@ -30,8 +30,8 @@ defmodule Townjudge do
   def find_judge(n, trust) do
     result =
       1..n
-      |> Enum.filter(&check_if_trusted_by_all(&1, trust, n))
-      |> Enum.filter(&check_if_not_trusting_others(&1, trust))
+      |> Enum.filter(&is_trusted_by_all(&1, trust, n))
+      |> Enum.filter(&is_not_trusting_others(&1, trust))
 
     case result do
       [] -> -1
